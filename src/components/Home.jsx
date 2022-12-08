22b47611d9f6e061ef5e01736def7b759afdb645
alagarsamykavn1521 <alagarsamy.k@kaaviansys.com>
2022-12-08 12:55:10 +0530

    react-dom

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card";
+import CardData from "./CardData";
+const Home = () => {
+    return (
+        <>
+            <div className="home-cards">
+                <Card details={CardData} />
+            </div>
+        </>
+    )
+}
+
+export default Home;
